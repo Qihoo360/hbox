aaf35f916a65a9e5ad19e415e8a1645ab1110823
jiarunying <hbrnwsxj@163.com>
2018-02-05 12:25:37 +0800

    update headerblock of history

--- src/main/java/net/qihoo/xlearning/jobhistory/HeaderBlock.java
+++ src/main/java/net/qihoo/xlearning/jobhistory/HeaderBlock.java
@@ -1,6 +1,6 @@
 package net.qihoo.xlearning.jobhistory;
 
-import org.apache.hadoop.yarn.webapp.hamlet.Hamlet.DIV;
+import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet.DIV;
 import org.apache.hadoop.yarn.webapp.view.HtmlBlock;
 
 public class HeaderBlock extends HtmlBlock {
@@ -13,6 +13,6 @@ public class HeaderBlock extends HtmlBlock {
       loggedIn = "Logged in as: " + this.request().getRemoteUser();
     }
 
-    ((DIV) ((DIV) html.div("#header.ui-widget").div("#user")._(new Object[]{loggedIn})._()).div("#logo").img("/static/xlWebApp/logo.png")._()).h1(this.$("title"))._();
+    ((DIV) ((DIV) html.div("#header.ui-widget").div("#user").__(new Object[]{loggedIn}).__()).div("#logo").img("/static/xlWebApp/logo.png").__()).h1(this.$("title")).__();
   }
 }
