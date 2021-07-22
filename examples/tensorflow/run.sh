420ffecca6d1019b9d26ff7809b0e99ae3e96a06
183570397@qq.com <lixiang>
2021-07-23 01:43:51 +0800

    tensorflow分布式demo, 原库中demo跑不同

--- examples/tensorflow/run.sh
+++ examples/tensorflow/run.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
+
+
+python_path="./anaconda3/bin/python"
+#python_path="./py2.7/bin/python"
+#python_path="python"
 $XLEARNING_HOME/bin/xl-submit \
-   --app-type "tensorflow" \
-   --app-name "tf-demo" \
-   --input /tmp/data/tensorflow#data \
-   --output /tmp/tensorflow_model#model \
-   --files demo.py,dataDeal.py \
-   --launch-cmd "python demo.py --data_path=./data --save_path=./model --log_dir=./eventLog --training_epochs=10" \
-   --worker-memory 10G \
-   --worker-num 2 \
-   --worker-cores 3 \
-   --ps-memory 2G \
-   --ps-num 1 \
-   --ps-cores 2 \
-   --queue default \
+    --app-type "tensorflow" \
+    --app-name "tf-demo" \
+    --input /data/tmp#data \
+    --output ./tensorflow_model#model \
+    --files demo.py,dataDeal.py \
+    --launch-cmd "${python_path} demo.py --data_path=./data --save_path=./model --log_dir=./eventLog --training_epochs=10" \
+    --worker-memory 25G \
+    --worker-num 2 \
+    --worker-cores 3 \
+    --ps-memory 25G \
+    --ps-num 1 \
+    --ps-cores 2 \
+	--queue hs2 \
+    --cacheArchive "./anaconda3.tar.gz#anaconda3,./tensorflow2.2.0.tar.gz#tensorflow" 
