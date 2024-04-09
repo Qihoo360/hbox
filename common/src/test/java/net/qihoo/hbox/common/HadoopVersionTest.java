b64bfb1c5aa2a853450051ce07dbf883d98000dc
gaozhiming <gaozhiming@360.cn>
2024-04-09 15:26:04 +0800

    add HadoopVersion to compare hadoop version, switch junit to 5

---
+++ common/src/test/java/net/qihoo/hbox/common/HadoopVersionTest.java
@@ -0,0 +1,24 @@
+package net.qihoo.hbox.common;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
+public class HadoopVersionTest {
+  @Test
+  public void testParseHadoopVersion() {
+    assertTrue(HadoopVersion.hasHaddopVersion());
+
+    // assume the hadoop version at build time is 3.2.1
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(3));
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(3, 2));
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(3, 2, 1));
+
+    assertFalse(HadoopVersion.isHaddopVersionAtLeast(4));
+    assertFalse(HadoopVersion.isHaddopVersionAtLeast(3, 3));
+    assertFalse(HadoopVersion.isHaddopVersionAtLeast(3, 2, 2));
+
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(2, 6));
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(2, 7));
+  }
+}
