1b09c3d49b26b7ad2e0b2004ed3c37769252a23b
jiangxinglei <jiangxinglei@360.cn>
2024-05-07 10:05:47 +0800

    update script content

--- tests/test-mpi/hpc-yarn.sh
+++ tests/test-mpi/hpc-yarn.sh
@@ -7,18 +7,11 @@ set -euo pipefail
 : "${HBOX_CONF_DIR:="$(dirname -- "$0")"/../conf.hpc-yarn}"
 export HBOX_CONF_DIR
 
-submit_opts=(--app-name "[HBOX][test] yarn submit")
+submit_opts=(--app-name "[HBOX][test] submit a mpi application")
 submit_opts+=(--app-type  "TENSORNET")
 submit_opts+=(--worker-num 2)
 submit_opts+=(--worker-cores 2)
 submit_opts+=(--worker-memory 8G)
 
-if [[ ${USE_LOCAL_MPI-} == true ]]; then
-  submit_opts+=(--conf hbox.use.cached.mpi.package=false)
-fi
-
-# command run on containers
-submit_opts+=(--hbox-cmd)
-submit_opts+=("/bin/sh -xc hostname;pwd;whoami;{ls,-l,.,tmp};{ps,f};cat<launch_container.sh;{which,hadoop};env")
-
-exec "$HBOX_HOME"/bin/hbox-submit "${submit_opts[@]}"
+exec "$HBOX_HOME"/bin/hbox-submit "${submit_opts[@]}" \
+ /bin/sh -xc 'hostname;pwd;whoami;{ls,-l,.,tmp};{ps,f};cat<launch_container.sh;{which,hadoop};env'
