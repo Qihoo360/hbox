57659c3e18023ae7c498c759eb4a735ced1bacab
gaozhiming <gaozhiming@360.cn>
2024-05-09 14:36:56 +0800

    fix shellcheck

--- tests/test-complex/hpc-yarn.sh
+++ tests/test-complex/hpc-yarn.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 [[ ${DEBUG-} != true ]] || set -x
 
 if [[ ! ${HBOX_HOME-} ]]; then
-  . "$(dirname -- "$0")/../ver.sh" # set HBOX_VERSION
+  # shellcheck disable=SC1091
+  . "$(dirname -- "$0")"/../ver.sh # set HBOX_VERSION
   HBOX_HOME="$(dirname -- "$0")/../../hbox-$HBOX_VERSION"
 fi
 
--- tests/test-history-server/hpc-yarn.sh
+++ tests/test-history-server/hpc-yarn.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 [[ ${DEBUG-} != true ]] || set -x
 
 if [[ ! ${HBOX_HOME-} ]]; then
+  # shellcheck disable=SC1091
   . "$(dirname -- "$0")/../ver.sh" # set HBOX_VERSION
   HBOX_HOME="$(dirname -- "$0")/../../hbox-$HBOX_VERSION"
 fi
