ecd23c986b710d4dad9a8c9e80b2372339618a1d
gaozhiming <gaozhiming@360.cn>
2024-06-11 18:04:31 +0800

    bump version for hadoop, aws-sdk and json

--- common/src/test/java/net/qihoo/hbox/common/HadoopVersionTest.java
+++ common/src/test/java/net/qihoo/hbox/common/HadoopVersionTest.java
@@ -9,14 +9,14 @@ public class HadoopVersionTest {
   public void testParseHadoopVersion() {
     assertTrue(HadoopVersion.hasHaddopVersion());
 
-    // assume the hadoop version at build time is 3.2.1
+    // assume the hadoop version at build time is 3.2.4
     assertTrue(HadoopVersion.isHaddopVersionAtLeast(3));
     assertTrue(HadoopVersion.isHaddopVersionAtLeast(3, 2));
-    assertTrue(HadoopVersion.isHaddopVersionAtLeast(3, 2, 1));
+    assertTrue(HadoopVersion.isHaddopVersionAtLeast(3, 2, 4));
 
     assertFalse(HadoopVersion.isHaddopVersionAtLeast(4));
     assertFalse(HadoopVersion.isHaddopVersionAtLeast(3, 3));
-    assertFalse(HadoopVersion.isHaddopVersionAtLeast(3, 2, 2));
+    assertFalse(HadoopVersion.isHaddopVersionAtLeast(3, 2, 5));
 
     assertTrue(HadoopVersion.isHaddopVersionAtLeast(2, 6));
     assertTrue(HadoopVersion.isHaddopVersionAtLeast(2, 7));
